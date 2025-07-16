5f9ac442ca12d0747f2fd2cfdaae0e60d7256d05
Nguyen Minh Dang <minhdangb3@gmail.com>
2025-07-16 22:49:25 +0700

    [DangNM] done code fe for microservice project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,16 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {routes} from './app.routes';
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import {authConfig} from "./config/auth.config";
+import {provideAuth} from "angular-auth-oidc-client";
+import {authInterceptor} from "./interceptor/auth.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+  providers: [provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideAuth(authConfig),
   ]
 };
